15c180e97a10ba13d31e8a379ac7000b01a001e1
Paolo Calao <paolo.calao@gmail.com>
2022-02-01 12:43:22 +0100

    Add unowifirev2 to known boards

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 var Avrgirl = require('avrgirl-arduino');
 
-
-var unowifirev2 = {
-  name: 'uno',
-  baud: 57600,
-  signature: new Buffer([0x1E, 0x96, 0x51]),
-  pageSize: 128,
-  numPages: 256,
-  timeout: 400,
-  productId: ['0x2145'],
-  productPage: 'https://store.arduino.cc/arduino-uno-wifi-rev2',
-  protocol: 'stk500v1'
-};
-
 var test = [
   {
-    board: unowifirev2,
+    board: 'unowifirev2',
     sketch: 'Unowifirev2-Blink.ino.hex'
   },
   {
